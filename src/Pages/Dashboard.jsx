777b0f2f6c75bd53007281b4b8968b0adf4305ca
mohit nandan <mohitnandan81825@gmail.com>
2024-07-03 00:11:55 +0530

    implment Dashboard page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import Appbar from "../Components/Appbar"
+import Balance from "../Components/Balance"
+import Users from "../Components/Users"
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    return <div>
+    <Appbar />
+    <div className="m-8">
+        <Balance value={"10,000"} />
+        <Users />
+    </div>
+</div>
+
 }
 
 export default Dashboard
+
